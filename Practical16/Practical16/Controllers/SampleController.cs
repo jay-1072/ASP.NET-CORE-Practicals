5017d12a6e3413b4eb2927264538e3f129d91860
Jay Koshti <jay.koshti@simformsolutions.com>
2023-07-10 12:16:52 +0530

    Add Practical16

---
+++ Practical16/Practical16/Controllers/SampleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practical16.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SampleController : ControllerBase
+{
+    private readonly ILogger<SampleController> _logger;
+
+    public SampleController(ILogger<SampleController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task Get()
+    {
+        _logger.LogInformation("Hello World API Print From End Point : " + HttpContext.Request.Path);
+        _logger.LogInformation("Hello World API Request Method : " + HttpContext.Request.Method);
+        _logger.LogInformation("Hello World API Request Host Name : " + HttpContext.Request.Host);
+        _logger.LogInformation("Hello World API Request is Https( Use SSL ) ? : " + HttpContext.Request.IsHttps);
+        _logger.LogInformation("Hello World API Request Protocol : " + HttpContext.Request.Protocol);
+        await HttpContext.Response.WriteAsync("Hello World");
+    }
+}
